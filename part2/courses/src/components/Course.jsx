6f83bae99000d41e804122591e3f6f58a8b737f0
Tiago Rodrigues <tiagopbr.2001@gmail.com>
2023-09-16 13:05:39 +0100

    Complete courses exercise

---
+++ part2/courses/src/components/Course.jsx
@@ -0,0 +1,48 @@
+const Header = ({ course }) => {
+  return (
+    <>
+      <h1>{course}</h1>
+    </>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <>
+      {parts.map((part) => (
+        <Part key={part.id} part={part} />
+      ))}
+    </>
+  );
+};
+
+const Part = ({ part }) => {
+  return (
+    <>
+      <p>
+        {part.name} {part.exercises}
+      </p>
+    </>
+  );
+};
+
+const Total = ({ parts }) => {
+  const total = parts.reduce((acc, curr) => acc + curr.exercises, 0);
+  return (
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  );
+};
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  );
+};
+
+export default Course;
